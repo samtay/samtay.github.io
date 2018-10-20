0d9ed75b54a905b4b322f9047860077ecc0d9e7c
Sam Tay <sam.chong.tay@gmail.com>
2018-10-20 17:09:12 -0400

    Try stack 1.7.1

---
+++ .circleci/deploy.sh
@@ -0,0 +1,21 @@
+git config --global user.email autodeploy@circleci.com
+git config --global user.name CircleCI
+
+cd _site
+
+if [[ $(git status --porcelain) ]]; then
+  git add --all
+  git commit -m "Update build (`date '+%F %T %Z'`) [ci skip]"
+  git push origin master
+fi
+
+cd ..
+
+git add _site
+if [[ $(git status --porcelain) ]]; then
+  git commit -m "Update _site (`date '+%F %T %Z'`) [ci skip]"
+  git push origin hakyll
+  echo "Deployed site successfully"
+else
+  echo "No changes to deploy"
+fi
